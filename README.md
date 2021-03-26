79b32102ae2791a7708b987e7bfdeea27c586135
sayem <sayem@grayspaceit.com>
2021-03-26 12:19:26 +0600

    APi url changed

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 How to install
 
 Clone the repo
+
 npm i
+
 npm run start
+
--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ let API_URL = "";
 if (process.env.NODE_ENV === "production") {
     API_URL = "https://filtering-demo.herokuapp.com";
 } else {
-    API_URL = "http://localhost:8000";
+    // API_URL = "http://localhost:8000";
+    API_URL = "https://filtering-demo.herokuapp.com";
 }
 
 export { API_URL };
